12789fa09b099a2fe7b7badc19c74eda833c7e26
Stanley <stanleypl75@gmail.com>
2021-08-18 19:31:36 -0400

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Normalize />
       <div>App Placeholder</div>
-    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/GlobalStyle.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    background-color: black;
+  }
+  `;
+export default GlobalStyle;
