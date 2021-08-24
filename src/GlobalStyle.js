0fff2cfbce8a5f6ca9fd12dc8cf7d266ad89f131
Stanley <stanleypl75@gmail.com>
2021-08-24 17:13:54 -0400

    Move to utils

--- src/GlobalStyle.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    font-family: 'Bitter', serif;
-    font-family: 'Montserrat', sans-serif
-  }
-  `;
-export default GlobalStyle;
