64ca0772593297b6a7c3e848706c7b7abaf1d906
Stanley <stanleypl75@gmail.com>
2021-08-25 13:22:12 -0400

    Add navbar navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Container = styled.header`
   display: flex;
@@ -14,7 +15,39 @@ const Container = styled.header`
     margin-right: 10px;
   }
 `;
+const NavListLink = styled(Link)`
+  color: ${(props) => props.theme.color.text};
+  padding-left: 26px;
+  text-decoration: none;
+  color: ${(props) => props.theme.color.text};
+`;
+const NavListItem = styled(Link)`
+  text-decoration: none;
+`;
+const NavBar = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
 
 export default function Header() {
-  return <Container>header</Container>;
+  return (
+    <Container>
+      <NavBar>
+        <NavListItem>
+          <NavListLink to="/search">Search</NavListLink>
+        </NavListItem>
+        <NavListItem>
+          <NavListLink to="/#how-it-works">How it works</NavListLink>
+        </NavListItem>
+        <NavListItem>
+          <NavListLink to="/#about">About</NavListLink>
+        </NavListItem>
+      </NavBar>
+    </Container>
+  );
 }
