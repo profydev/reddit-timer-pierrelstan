ed9322b8e3ebe03c9909c3b4a6212f18c46dbc42
Stanley <stanleypl75@gmail.com>
2021-08-28 11:33:09 -0400

    Create home page

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>home page</h1>
+      <div id="about">about</div>
+      <div id="how-it-works">How it works</div>
+    </div>
+  );
+}
