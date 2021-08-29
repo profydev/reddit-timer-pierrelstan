80ad799cf9e79d64b62781bac18f0cbefa49681e
Stanley  Pierre-Louis <stanleypl75@gmail.com>
2021-08-29 17:01:54 -0400

    Create app skeleton  (#1)
    * Install fonts from Google fonts
    
    * Reset default browser styles
    
    * Add global styles
    
    * Add router with routes/ and /search
    
    * Add global fonts
    
    This reverts commit 12789fa09b099a2fe7b7badc19c74eda833c7e26.
    
    * Change app title and meta description
    
    * Change the logos
    
    * Add fonts
    
    * Add globalStyle & theme
    
    * Create NoMatch
    
    * Update  links GlobalStyle & theme
    
    * Add NoMatch
    
    * Move to utils
    
    * Move to utils
    
    * Configure theme
    
    * Add font-face   / default props theme
    
    * Add env
    
    * Update yarn
    
    * Create Home
    
    * Create Search
    
    * Add Home and  Search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import NoMatch from './components/NoMatch';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import GlobalStyle from './utils/GlobalStyle';
+import theme from './utils/theme';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Normalize />
+        <GlobalStyle />
+        <Switch>
+          <Route path="search">
+            <Search />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
-
 export default App;
---
+++ src/components/NoMatch.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NoMatch() {
+  return (
+    <div>
+      <h3>404 -Not Found</h3>
+    </div>
+  );
+}
