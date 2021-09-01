a6c9de27b43545d75ef2b15c452fad800e67c0f4
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-01 05:58:59 -0400

    Add test  footer links

---
+++ src/components/Footer/Footer.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../utils/theme';
+
+import Footer from './Footer';
+
+const setup = () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <MemoryRouter>
+        <Footer />
+      </MemoryRouter>
+    </ThemeProvider>,
+  );
+};
+
+describe('Testing Footer links', () => {
+  test('navigates to profy.dev/employers when profy.dev is clicked', () => {
+    setup();
+    expect(
+      screen.getByText(/profy.dev/i).closest('a'),
+    ).toHaveAttribute('href', 'https://profy.dev/employers');
+  });
+
+  test('navigates to home when logo is clicked', () => {
+    setup();
+    expect(
+      screen.getByText(/logo.svg/i).closest('a'),
+    ).toHaveAttribute('href', '/');
+  });
+
+  test('navigates to terms  page when Terms & Privacy link is clicked', () => {
+    setup();
+    expect(
+      screen.getByText(/terms & privacy/i).closest('a'),
+    ).toHaveAttribute('href', '/terms');
+  });
+});
