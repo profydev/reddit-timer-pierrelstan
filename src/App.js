2f8dbd5c2bd6c5c23592323f79ed657ead8caf59
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-01 01:46:57 -0400

    Add footer /  className container & wrapper

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import NoMatch from './components/NoMatch/NoMatch';
 import Home from './pages/Home';
@@ -11,24 +16,29 @@ import theme from './utils/theme';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Normalize />
-        <GlobalStyle />
-        <Header />
-        <Switch>
-          <Route exact path="/search/javascript">
-            <Search />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="*">
-            <NoMatch />
-          </Route>
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <div className="container">
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Normalize />
+          <GlobalStyle />
+          <div className="wrapper">
+            <Header />
+            <Switch>
+              <Route exact path="/search/javascript">
+                <Search />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+              <Route exact path="*">
+                <NoMatch />
+              </Route>
+            </Switch>
+          </div>
+          <Footer />
+        </Router>
+      </ThemeProvider>
+    </div>
   );
 }
 export default App;
