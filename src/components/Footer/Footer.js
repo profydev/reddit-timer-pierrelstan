2ed813cb6cb3f153180ea550e00ba9653db73f3f
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-06 21:33:33 -0400

    Add external link / tothright / remove List

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,25 +3,24 @@ import {
   Container,
   Logo,
   LogoLink,
-  List,
   ListItem,
 } from './Footer.style';
 
 export default function Footer() {
   return (
     <Container>
-      <List>
-        <ListItem
-          to={{ pathname: 'https://profy.dev/employers' }}
-          target="_blank"
-        >
-          profy.dev
-        </ListItem>
-        <LogoLink to="/">
-          <Logo />
-        </LogoLink>
-        <ListItem to="/terms">Terms &amp; Privacy</ListItem>
-      </List>
+      <ListItem
+        as="a"
+        href="https://profy.dev/employers"
+      >
+        profy.dev
+      </ListItem>
+      <LogoLink to="/">
+        <Logo />
+      </LogoLink>
+      <ListItem to="/terms" totheright>
+        Terms &amp; Privacy
+      </ListItem>
     </Container>
   );
 }
