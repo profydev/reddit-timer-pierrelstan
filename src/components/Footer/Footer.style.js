79bc901de12c20e8925364d83c1a219cab571bf2
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-06 21:35:48 -0400

    Update Container / ListItem / Logo

--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -3,39 +3,19 @@ import { Link } from 'react-router-dom';
 import { ReactComponent } from './sign.svg';
 
 export const Container = styled.footer`
-  display: inline-grid;
-  height: 80px;
+  display:flex;
   width: 100%;
-  @media (max-width: 768px) {
-    flex-direction: column;
-    height: 75px;
-  }
-  @media (max-width: 358px) {
-    margin-left: 5px;
-    margin-right: 5px;
-    height: 75px;
-  }
-`;
+  max-width: 940px;
+  margin: auto;
+  padding: 0 20px;`;
 
 export const ListItem = styled(Link)`
+  flex:1;
   color: ${(props) => props.theme.color.text};
-  font-size: 14px;
+  font-size: ${(props) => props.theme.font.size.small};
+  text-align: ${({ totheright }) => (totheright ? 'right ' : 'unset')};
   text-decoration: none;
-  margin: 0;
-  font-weight: 300;
-  padding: 0;
-  color: ${(props) => props.theme.color.text};
-  @media (max-width: 400px) {
-    font-size: 10px;
-  }
-`;
-
-export const List = styled.ul`
-  display: grid;
-  grid-template-columns: 40% 1fr 40%;
-  justify-items: center;
-  padding: 0;
-`;
+  padding-top: ${({ totheright }) => (totheright ? '7px' : '8px')}`;
 
 export const LogoLink = styled(Link)`
   padding: 0;
@@ -43,7 +23,4 @@ export const LogoLink = styled(Link)`
 `;
 
 export const Logo = styled(ReactComponent)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
 `;
