b0fec3eb558463a8687784725ab3c1a9513a06f7
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-11 19:12:16 -0400

    Add Container / TableImage

---
+++ src/components/HeroSection/HeroSection.style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { ReactComponent } from './table.svg';
+
+export const Container = styled.div`
+padding-top: 100px`;
+
+export const TableImage = styled(ReactComponent)`
+`;
