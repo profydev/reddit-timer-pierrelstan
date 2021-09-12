ba9e3519e56c39f6387dd2af143a1803ae81089f
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-12 15:43:14 -0400

    Update Container

--- src/components/HeroSection/HeroSection.style.js
+++ src/components/HeroSection/HeroSection.style.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { ReactComponent } from './table.svg';
 
 export const Container = styled.div`
-padding-top: 100px`;
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+width: 100%;
+padding-top: 100px ;
+`;
 
 export const TableImage = styled(ReactComponent)`
 `;
