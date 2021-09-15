bebea31688d70c465c8196f063f61a6e6006d46f
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-15 17:22:26 -0400

    Add Container / Wrapper / Title / SubTitle

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import * as S from './HeroSection.style';
 
 export default function HeroSection() {
   return (
-  <div>Hero Section</div>
+    <S.Container>
+      <S.Wrapper>
+        <S.Title>No reactions to your reddit posts?</S.Title>
+        <S.SubTitle>
+          Great timing, great results! Find the best time to post on
+          your subreddit.
+        </S.SubTitle>
+      </S.Wrapper>
+    </S.Container>
   );
 }
