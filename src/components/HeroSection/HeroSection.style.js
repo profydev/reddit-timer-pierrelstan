31c5ef5bab4c58ff625ff2bdad515956072f8d56
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-15 17:05:14 -0400

    Add Container / Wrapper

--- src/components/HeroSection/HeroSection.style.js
+++ src/components/HeroSection/HeroSection.style.js
@@ -1,1 +1,11 @@
+import styled from 'styled-components';
 
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+width: 100%;
+padding-top: 127px ;`;
+
+export const Wrapper = styled.div`
+text-align:center;`;
