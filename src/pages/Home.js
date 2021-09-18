bf8ea9b76c938856d2119e41846fbf0579db7cf6
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-18 19:37:04 -0400

    Add InfoSection / scrollToLocation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import HeroSection from '../components/HeroSection/HeroSection';
+import InfoSection from '../components/InfoSection/InfoSection';
+import scrollToLocation from '../utils/ScrollToLocation';
 
 export default function Home() {
+  const location = useLocation();
+
+  useEffect(() => {
+    scrollToLocation(location);
+  }, [location]);
+
   return (
     <div>
       <HeroSection />
-      <div id="about">about</div>
-      <div id="how-it-works">How it works</div>
+      <InfoSection />
     </div>
   );
 }
