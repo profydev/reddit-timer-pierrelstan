694bf78e5b9b72425cfa0eb764c074dc14b91930
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-18 19:28:45 -0400

    Add Container / LinkProfy

---
+++ src/components/InfoSection/InfoSection.style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Container = styled.section`
+width: 100%;
+max-width: 770px;
+height: 457px;
+margin:auto;
+padding: 0px 20px;
+`;
+
+export const LinkProfy = styled(Link)`
+color:#3c88d5;
+text-decoration: none;`;
