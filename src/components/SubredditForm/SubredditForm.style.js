864591d1f786f45343789d44d10b03ccc93fc8a5
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-22 17:39:49 -0400

    Add Input / Button

--- src/components/SubredditForm/SubredditForm.style.js
+++ src/components/SubredditForm/SubredditForm.style.js
@@ -28,3 +28,31 @@ color: ${({ theme }) => theme.color.dark};
 margin-top: 27px;
 margin-bottom: 22px;
 font-weight: 400`;
+
+export const Input = styled.input`
+width: 336px;
+height: 34px;
+padding: 0px 17px;
+font-size: ${({ theme }) => theme.font.size.small};
+border-radius: 2px;
+border: 1px solid ${({ theme }) => theme.color.midLight};
+background-color: ${({ theme }) => theme.color.light};
+margin-right: 10px;
+margin-bottom: 0;
+margin-left:0;
+margin-top: 0;
+line-height: 1.15;
+`;
+
+export const Button = styled.button`
+padding: 14px 16px;
+border-radius: 4px;
+font-size: ${({ theme }) => theme.font.size.small};
+font-weight: 500;
+line-height: 0.64;
+background-color: #FDB755;
+color: ${({ theme }) => theme.color.light};
+text-transform: uppercase;
+border: none;
+outline: none;
+`;
