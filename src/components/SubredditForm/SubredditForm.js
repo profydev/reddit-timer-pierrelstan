8d51202b95c6b5271a4424b8d3e19fa1e6d9a68b
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-22 17:35:49 -0400

    Configure defaultValue / onChange / onSubmit

--- src/components/SubredditForm/SubredditForm.js
+++ src/components/SubredditForm/SubredditForm.js
@@ -1,23 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function SubredditForm() {
-  const handleChange = () => {
+function SubredditForm({
+  state,
+  handleChangeState,
+  handleSubmitState,
+}) {
+  const handleChange = (e) => {
+    handleChangeState(e);
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    handleSubmitState(e);
   };
-
   return (
     <div>
       <h1>Find the best time for a subreddit</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor="search">
           r /
-          <input defaultValue="" onChange={handleChange} />
+          <input defaultValue={state} onChange={handleChange} />
         </label>
         <button type="submit">Search</button>
       </form>
     </div>
   );
 }
+SubredditForm.defaultProps = {
+  state: '',
+  handleChangeState: () => '',
+  handleSubmitState: () => '',
+};
+
+SubredditForm.propTypes = {
+  state: PropTypes.string,
+  handleChangeState: PropTypes.func,
+  handleSubmitState: PropTypes.func,
+};
 export default SubredditForm;
