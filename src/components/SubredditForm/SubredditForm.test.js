a7f47614eb1ffa41b0957eb3674b61a50e1fe375
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-23 21:46:51 -0400

    Update url with input value  on submit

---
+++ src/components/SubredditForm/SubredditForm.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  render, screen
+} from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import userEvent from '@testing-library/user-event';
+import App from '../../App';
+
+
+const setup = () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+};
+
+
+
+describe('SubredditForm', () => {
+
+  test('Updates URL with input value on submit', () => {
+    setup();
+
+    const SearchLink = screen.getByRole('link', { name: /search/i });
+
+    userEvent.click(SearchLink);
+
+    const subredditInput = screen.getByLabelText('r /');
+
+    userEvent.clear(screen.getByRole('textbox'));
+
+    userEvent.type(subredditInput, 'reactjs');
+
+    const submitButton = screen.getByRole('button', { name: /search/i });
+
+    userEvent.click(submitButton);
+
+    const value = screen.getByLabelText('r /');
+
+    expect(value.value).toBe('reactjs');
+
+  });
+});
+
