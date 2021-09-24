c836a4d8d8175e3d753eca2ffd46f7ab9fa8ee82
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-24 19:02:50 -0400

    Input value update when the link is click.

--- src/components/SubredditForm/SubredditForm.test.js
+++ src/components/SubredditForm/SubredditForm.test.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import {
-  render, screen
+  render, screen,
 } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import userEvent from '@testing-library/user-event';
 import App from '../../App';
 
-
 const setup = () => {
   render(
     <MemoryRouter>
       <App />
-    </MemoryRouter>
+    </MemoryRouter>,
   );
 };
 
-
-
 describe('SubredditForm', () => {
-
   test('Updates URL with input value on submit', () => {
     setup();
 
@@ -39,7 +35,17 @@ describe('SubredditForm', () => {
     const value = screen.getByLabelText('r /');
 
     expect(value.value).toBe('reactjs');
+  });
 
+  test('Input value updates to "javascript" when header link is clicked', () => {
+    setup();
+
+    const SearchLink = screen.getByRole('link', { name: /search/i });
+
+    userEvent.click(SearchLink);
+
+    const input = screen.getByLabelText('r /');
+
+    expect(input.value).toBe('javascript');
   });
 });
-
