454abb2d6b664acec1cbed8c4644c37e784c07b9
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-23 21:37:18 -0400

    Configure state & url

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import SubredditForm from '../components/SubredditForm/SubredditForm';
 
 export default function Search() {
+  const history = useHistory();
+
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    const string = history.location.pathname.substr(8);
+    if (history.location.pathname === '/search/javascript') {
+      setSearch('javascript');
+    }
+    setSearch(string);
+  }, [history.location.pathname]);
+
+  const handleChangeState = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmitState = (e) => {
+    e.preventDefault();
+    const url = `/search/${search}`;
+    history.push(url);
+  };
+
   return (
     <div>
-      <SubredditForm />
+      <SubredditForm
+        handleChangeState={handleChangeState}
+        handleSubmitState={handleSubmitState}
+        onSearch={search}
+      />
     </div>
   );
 }
