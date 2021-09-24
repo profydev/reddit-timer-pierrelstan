466f9a1dcb80373d3ac86d7422892fa8c833bf3a
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-24 16:49:27 -0400

    Reconfigure state search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import SubredditForm from '../components/SubredditForm/SubredditForm';
 
 export default function Search() {
   const history = useHistory();
 
+  const id = useParams();
+
   const [search, setSearch] = useState('');
 
   useEffect(() => {
-    const string = history.location.pathname.substr(8);
-    if (history.location.pathname === '/search/javascript') {
-      setSearch('javascript');
-    }
-    setSearch(string);
-  }, [history.location.pathname]);
+    setSearch(id.search);
+  }, [id]);
 
   const handleChangeState = (e) => {
     setSearch(e.target.value);
@@ -24,13 +22,12 @@ export default function Search() {
     const url = `/search/${search}`;
     history.push(url);
   };
-
   return (
     <div>
       <SubredditForm
         handleChangeState={handleChangeState}
         handleSubmitState={handleSubmitState}
-        onSearch={search}
+        search={search}
       />
     </div>
   );
