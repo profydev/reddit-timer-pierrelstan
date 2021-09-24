33dfa811c0a56938d8390bd5dcc87eb0670cc8f2
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-24 17:14:41 -0400

    Add infoSection tests

---
+++ src/components/InfoSection/InfoSection.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { ThemeProvider } from 'styled-components';
+import userEvent from '@testing-library/user-event';
+import theme from '../../utils/theme';
+import InfoSection from './InfoSection';
+
+const setup = () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <MemoryRouter>
+      <InfoSection />
+      </MemoryRouter>
+    </ThemeProvider>,
+  );
+};
+
+
+describe('InfoSection', () => {
+  test('link profy.dev should have url "profy.dev" ', () => {
+    setup();
+    expect(
+      screen.getByText(/profy.dev/i).closest('a'),
+    ).toHaveAttribute('href', 'https://profy.dev');
+  });
+  test('Click here for more information should have url profy.dev/employers ', () => {
+    setup();
+    expect(
+      screen.getByText(/click here for more information/i).closest('a'),
+    ).toHaveAttribute('href', 'https://profy.dev/employers');
+  });
+});
