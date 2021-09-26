e16bae4eae8773c84cfc36f919c44e94e1184cbd
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-25 23:58:20 -0400

    Remove unnecessary test

--- src/components/HeroSection/HeroSection.test.js
+++ src/components/HeroSection/HeroSection.test.js
@@ -17,22 +17,4 @@ describe('HeroSection', () => {
       screen.getByText(/show me the best time/i).closest('a'),
     ).toHaveAttribute('href', '/search/javascript');
   });
-
-  test('Should contains  a title and  a subtile ', () => {
-    setup();
-    const title = screen.getByText(/No reactions to your reddit posts/i);
-    const subTitle = screen.getByText(/Great timing, great results. Find the best time to post on your subreddit/i);
-    expect(
-      title.closest('h1'),
-    ).toBeInTheDocument();
-    expect(
-      subTitle.closest('h3'),
-    ).toBeInTheDocument();
-  });
-  test('navigates to Search page when table Image is clicked', () => {
-    setup();
-    const tableLink = screen.getByRole('link', { name: /table.svg/i });
-    userEvent.click(tableLink);
-    expect(screen.getByText(/search page/i)).toBeInTheDocument();
-  });
 });
