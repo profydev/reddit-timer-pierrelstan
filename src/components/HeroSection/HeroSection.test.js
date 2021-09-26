9fe960a818910b7a5ea6f87e684e4ad85fd63870
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-26 00:01:08 -0400

    Remove userEvent

--- src/components/HeroSection/HeroSection.test.js
+++ src/components/HeroSection/HeroSection.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom/cjs/react-router-dom.min';
-import userEvent from '@testing-library/user-event';
 import App from '../../App';
 
 const setup = () => render(
