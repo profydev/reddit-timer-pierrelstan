d70fb89d281d87548c2c861cb1b4bdb330ad6c20
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-09-27 20:19:50 -0400

    Remove scrollToLocation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,8 @@
-import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import React from 'react';
 import HeroSection from '../components/HeroSection/HeroSection';
 import InfoSection from '../components/InfoSection/InfoSection';
-import scrollToLocation from '../utils/ScrollToLocation';
 
 export default function Home() {
-  const location = useLocation();
-
-  useEffect(() => {
-    scrollToLocation(location);
-  }, [location]);
-
   return (
     <div>
       <HeroSection />
