2b445b9e7d8ac1d0e4d6d1cf28c1fbe652d18184
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-10-09 22:30:14 -0400

    Create heatmap

---
+++ src/components/Heatmap/Heatmap/Heatmap.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Spinner from '../Spinner/Spinner';
+import * as S from './Heatmap.style';
+
+export default function Heatmap({ data, loading, error }) {
+  return (
+    <S.Container>
+      {error && <p>No such subreddit!!</p>}
+      {loading && <Spinner />}
+      <p>
+        {data.length}
+      </p>
+    </S.Container>
+  );
+}
+
+Heatmap.defaultsProps = {
+  data: [],
+  loading: false,
+  error: null,
+};
+
+Heatmap.propTypes = {
+  data: PropTypes.shape([]).isRequired,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string.isRequired,
+};
