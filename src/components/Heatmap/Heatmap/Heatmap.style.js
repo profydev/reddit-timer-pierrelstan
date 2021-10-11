df69825cb95d9fd77b0b296fe02138d8f7950e92
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-10-11 18:04:40 -0400

    Implement loading spinner style

--- src/components/Heatmap/Heatmap/Heatmap.style.js
+++ src/components/Heatmap/Heatmap/Heatmap.style.js
@@ -1,4 +1,30 @@
-import styled from 'styled-components';
-// eslint-disable-next-line import/prefer-default-export
-export const Container = styled.div`
-padding-top: 55px`;
+import styled, { keyframes } from 'styled-components';
+import { ReactComponent as unStyledSpinner } from './loading-spinner.svg';
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const LoadingSpinner = styled(unStyledSpinner)`
+animation: ${rotate} 1.5s linear infinite;
+}`;
+
+export const Container = styled.div``;
+
+export const LoadingContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 56px;
+`;
+export const ErrorContainer = styled.p`
+text-align:center;
+padding: 30px;
+color: red;
+font-size: ${(props) => props.theme.font.size.small};`;
