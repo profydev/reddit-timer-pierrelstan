a9efa6801781d5c6aae48c04c95be90dc430e0db
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-11-05 13:49:50 -0400

    Add mock service worker for the server

---
+++ src/__mocks__/server.js
@@ -0,0 +1,5 @@
+import { setupServer } from 'msw/node';
+import handlers from './handlers';
+// Setup requests interception using the given handlers.
+// eslint-disable-next-line import/prefer-default-export
+export const server = setupServer(...handlers);
