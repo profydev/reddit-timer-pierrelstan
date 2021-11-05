a5f8d6cd209bef54b4f7bc2ca34b41c8f93ca1be
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-11-05 17:16:24 -0400

    Make  worker browser start only in development

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line
+  const { worker } = require('./__mocks__/browser');
+  // console.log(worker);
+  worker.start();
+}
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
