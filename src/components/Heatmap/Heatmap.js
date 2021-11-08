e3f8e652f47b06c2a9367c7f77b31e07469fdd00
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-11-08 14:59:12 -0400

    Add table

--- src/components/Heatmap/Heatmap.js
+++ src/components/Heatmap/Heatmap.js
@@ -1,36 +1,89 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import * as S from './Heatmap.style';
+// import PropTypes from 'prop-types';
+// import * as S from './Heatmap.style';
 
-export default function Heatmap({ isLoading, hasError, Posts }) {
+export default function HeatmapSection() {
   return (
-    <S.Container>
-      {hasError && (
-      <S.ErrorContainer>
-        {' '}
-        Something went wrong.
-      </S.ErrorContainer>
-      )}
-      {isLoading && (
-      <S.LoadingContainer>
-        <S.LoadingSpinner data-testid="loading" />
-      </S.LoadingContainer>
-      )}
-      <div data-testid="500-posts">
-        {Posts.length}
-      </div>
-    </S.Container>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>`&quot;`</th>
+            <th>12:00am</th>
+            <th>2:00am</th>
+            <th>4:00am</th>
+            <th>6:00am</th>
+            <th>8:00am</th>
+            <th>10:00am</th>
+            <th>12:00pm</th>
+            <th>2:00pm</th>
+            <th>4:00pm</th>
+            <th>6:00pm</th>
+            <th>8:00pm</th>
+            <th>10:00pm</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Sunday</th>
+            <td>1</td>
+            <td>3</td>
+            <td>6</td>
+            <td>1</td>
+            <td>1</td>
+            <td>7</td>
+            <td>9</td>
+            <td>8</td>
+            <td>9</td>
+            <td>3</td>
+            <td>1</td>
+            <td>1</td>
+            <td>7</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+
+          </tr>
+          <tr>
+            <th>Monday</th>
+          </tr>
+          <tr>
+            <th>Tuesday</th>
+          </tr>
+          <tr>
+            <th>Wednesday</th>
+          </tr>
+          <tr>
+            <th>Thursday</th>
+          </tr>
+          <tr>
+            <th>Friday</th>
+          </tr>
+          <tr>
+            <th>Saturday</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
-Heatmap.defaultsProps = {
-  Posts: [],
-  isLoading: false,
-  hasError: false,
-};
+// HeatmapSection.defaultsProps = {
+//   Posts: [],
+//   isLoading: false,
+//   hasError: false,
+// };
 
-Heatmap.propTypes = {
-  Posts: PropTypes.arrayOf(PropTypes.object).isRequired,
-  isLoading: PropTypes.bool.isRequired,
-  hasError: PropTypes.bool.isRequired,
-};
+// HeatmapSection.propTypes = {
+//   Posts: PropTypes.arrayOf(PropTypes.object).isRequired,
+//   isLoading: PropTypes.bool.isRequired,
+//   hasError: PropTypes.bool.isRequired,
+// };
