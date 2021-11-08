c2f3e7074bca3b71183a00a10f576a00ac4d4a2b
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-11-08 14:51:34 -0400

    Add HeatmapSection

--- src/pages/search-page/Search.js
+++ src/pages/search-page/Search.js
@@ -4,7 +4,7 @@ import * as S from '../../components/SubredditForm/SubredditForm.style';
 import SubredditForm from '../../components/SubredditForm/SubredditForm';
 import Container from '../../common/container/index';
 import useFetchPosts from '../../Hooks/useFetchPosts';
-import Heatmap from '../../components/Heatmap/Heatmap';
+import HeatmapSection from '../../components/Heatmap/Heatmap';
 
 export default function Search() {
   const history = useHistory();
@@ -41,7 +41,7 @@ export default function Search() {
           values={values}
         />
       </S.Wrapper>
-      <Heatmap Posts={Posts} isLoading={isLoading} hasError={hasError} />
+      <HeatmapSection Posts={Posts} isLoading={isLoading} hasError={hasError} />
     </Container>
   );
 }
