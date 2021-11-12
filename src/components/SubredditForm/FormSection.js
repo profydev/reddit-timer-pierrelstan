90bc4c67e4ca5adf11af421eba8957b2205ceb21
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-11-12 17:13:55 -0400

    Add FormSection

---
+++ src/components/SubredditForm/FormSection.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SubredditForm from './SubredditForm';
+import * as S from './SubredditForm.style';
+
+export default function FormSection({ OnChange, OnSubmit, values }) {
+  return (
+    <S.Wrapper>
+      <S.Headline>Find the best time for a subreddit</S.Headline>
+      <SubredditForm
+        OnChange={OnChange}
+        OnSubmit={OnSubmit}
+        values={values}
+      />
+    </S.Wrapper>
+  );
+}
+FormSection.defaultsProps = {
+  OnSubmit: () => {},
+  OnChange: () => {},
+  values: '',
+};
+
+FormSection.propTypes = {
+  values: PropTypes.string.isRequired,
+  OnChange: PropTypes.func.isRequired,
+  OnSubmit: PropTypes.func.isRequired,
+};
