4fbf15b044c7f1c18cc9158f59cb9c443d2a3ad9
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-11-12 18:06:50 -0400

    Add Container / TimezoneWrapper / Timerzone

--- src/components/Heatmap/Heatmap.style.js
+++ src/components/Heatmap/Heatmap.style.js
@@ -15,7 +15,22 @@ export const LoadingSpinner = styled(unStyledSpinner)`
 animation: ${rotate} 1.5s linear infinite;
 }`;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: ${(props) => props.theme.font.size.heatmap.width}px;
+  margin: auto;
+  padding-top: 30px;
+`;
+
+export const TimezoneWrapper = styled.div`
+  width: 100%;
+  margin-top: 10px;
+  font-size: ${(props) => props.theme.font.size.small};
+  text-align: center;
+`;
+
+export const Timezone = styled.span`
+  font-weight: 600;
+`;
 
 export const LoadingContainer = styled.div`
   width: 100%;
