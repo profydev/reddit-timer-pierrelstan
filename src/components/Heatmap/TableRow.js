cd1d2aaca88c9bc43d963703ae941935ca679cdf
Stanley Pierre-Louis <stanleypl75@gmail.com>
2021-11-20 08:39:21 -0400

    Add PostsAuthor

--- src/components/Heatmap/TableRow.js
+++ src/components/Heatmap/TableRow.js
@@ -4,29 +4,20 @@ import {
   string,
 } from 'prop-types';
 import * as S from './PostsTable.style';
+import PostsAuthor from './PostsAuthor';
 
 export default function TableRow({
   date, score, numComments, author,
 }) {
   return (
     <>
-      <td>{date}</td>
-      <td>{score}</td>
-      <td>{numComments}</td>
+      <S.Column>{date}</S.Column>
+      <S.Column>{score}</S.Column>
+      <S.Column>{numComments}</S.Column>
+      <S.AuthorColumn>
+        <PostsAuthor author={author} />
+      </S.AuthorColumn>
 
-      <td>
-        {author !== '[deleted]'
-          ? (
-            <S.Link
-              href={`https://reddit.com/u/${author}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {author}
-            </S.Link>
-          )
-          : author}
-      </td>
     </>
   );
 }
